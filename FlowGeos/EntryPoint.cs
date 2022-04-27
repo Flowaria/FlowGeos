a681c0574882f3c2ef4791b0d13e113fabd907e7
Flowaria <wnsghgj1234@naver.com>
2022-04-27 18:46:28 +0900

    Add project files.

---
+++ FlowGeos/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+
+namespace FlowGeos
+{
+    [BepInPlugin("FlowGeos", "FlowGeos", "1.0.0")]
+    public class EntryPoint : BasePlugin
+    {
+        // The method that gets called when BepInEx tries to load our plugin
+        public override void Load()
+        {
+            // Creates a new harmony instance to allow for patching into methods
+            m_Harmony = new Harmony("FlowGeos");
+            // Apply all patches in the current assembly
+            m_Harmony.PatchAll();
+
+            // Log to the console that we have applied all patches
+            FlowGeosLogger.Verbose($"Applied all patches!");
+        }
+
+        private Harmony m_Harmony;
+    }
+}
