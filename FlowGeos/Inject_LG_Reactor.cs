261e596bf6cc7b5a97ca7e71a629037be5a5e385
Flowaria <wnsghgj1234@naver.com>
2022-05-25 23:27:47 +0900

    Updated FlowGeos Plugin source

---
+++ FlowGeos/Inject_LG_Reactor.cs
@@ -0,0 +1,26 @@
+﻿using GTFO.API;
+using HarmonyLib;
+using LevelGeneration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace FlowGeos
+{
+    [HarmonyPatch(typeof(LG_WardenObjective_Reactor), nameof(LG_WardenObjective_Reactor.GenericObjectiveSetup))]
+    static class Inject_LG_Reactor
+    {
+        const string TERMINAL_PREFAB = "ASSETS/ASSETPREFABS/COMPLEX/GENERIC/FUNCTIONMARKERS/TERMINAL_FLOOR.PREFAB";
+
+        static void Prefix(LG_WardenObjective_Reactor __instance)
+        {
+            if (__instance.m_terminalPrefab == null)
+            {
+                var ter = AssetAPI.GetLoadedAsset<GameObject>(TERMINAL_PREFAB);
+                __instance.m_terminalPrefab = ter;
+                FlowGeosLogger.Info("Terminal Prefab Resolved!");
+            }
+        }
+    }
+}
